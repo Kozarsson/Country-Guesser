0d5a9f113902ad227cf5f5a7c730c73b8392d749
Bernard Rumar <rumarbernard@gmail.com>
2026-05-05 20:20:30 +0200

    change rendering method

--- app/src/main/java/org/kth/countryguesser/view/components/Map.kt
+++ app/src/main/java/org/kth/countryguesser/view/components/Map.kt
@@ -1,89 +1,94 @@
 package org.kth.countryguesser.view.components
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fitOutside
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.drawscope.draw
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.scale
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.IntSize
 import org.kth.countryguesser.R
 
 @Composable
 fun Map() {
-    val painter = painterResource(id = R.drawable.world)
-
-    InteractiveMap(
-        painter = painter,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xff70d6ef)),
-    )
-}
-
-@Composable
-private fun InteractiveMap(
-    painter: Painter,
-    modifier: Modifier = Modifier,
-) {
-    var scale by remember { mutableFloatStateOf(1f) }
+    var scale by remember { mutableFloatStateOf(4.75f) }
     var offset by remember { mutableStateOf(Offset.Zero)}
     var mapSize by remember { mutableStateOf(IntSize(0,0)) }
 
+    val mapVector : ImageVector = ImageVector.vectorResource(id = R.drawable.world)
+    val vectorPainter = rememberVectorPainter(mapVector)
+
     Box(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
             .clip(RectangleShape)
+            .background(Color(0xff70d6ef))
             .onSizeChanged { mapSize = it }
             .pointerInput(Unit) {
                 detectTransformGestures { centroid, pan, zoom, _ /* no rotation */ ->
-
-                    scale = (scale * zoom).coerceIn(1f, 10f)
+                    scale = (scale * zoom).coerceIn(4.5f, 100f)
 
                     // don't pan out of bounds
                     val maxX = (mapSize.width * (scale - 1)).coerceAtLeast(0f)
-                    val maxY = (mapSize.width * (scale - 1)).coerceAtLeast(0f)
+                    val maxY = (mapSize.height * (scale - 1)).coerceAtLeast(0f)
 
                     // set zoom at center of finger pinch
-                    if (scale < 10f) {
-                        offset = (offset * zoom) + (centroid - centroid * zoom) + pan
-                    }
+                    offset = (offset * zoom) + (centroid - centroid * zoom) + pan
                     // don't pan out of bounds
                     offset = Offset(
                         x = offset.x.coerceIn(-maxX, 0f),
                         y = offset.y.coerceIn(-maxY, 0f),
                     )
                 }
-            }
+            },
     ) {
-        Image(
-            painter = painter,
-            contentDescription = "World map",
-            modifier = Modifier
-                .fillMaxSize()
-                .graphicsLayer(
-                    scaleX = scale,
-                    scaleY = scale,
-                    translationX = offset.x,
-                    translationY = offset.y,
-                    transformOrigin = TransformOrigin(0f, 0f), // set offset origin to same as scale origin
-                ),
-        )
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val aspect = mapVector.defaultWidth.value / mapVector.defaultHeight.value
+            val baseHeight = minOf(size.height, size.width / aspect)
+            val baseWidth = minOf(size.width, size.height * aspect)
+
+            val scaleX = baseWidth / mapVector.defaultWidth.value
+            val scaleY = baseHeight / mapVector.defaultHeight.value
+
+            with(drawContext.canvas) {
+                save()
+                translate(offset.x, offset.y)
+
+                scale(scale, scale, pivotX = 0f, pivotY = 0f)
+                scale(scaleX, scaleY, pivotX = 0f, pivotY = 0f)
+
+                with(vectorPainter) {
+                    draw(size = Size(mapVector.defaultWidth.value, mapVector.defaultHeight.value))
+                }
+                restore()
+            }
+        }
     }
 }
