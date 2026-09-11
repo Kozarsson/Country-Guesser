fc66c87fe0628fd2adfbd7e6a7d21d3f65f27633
Matyas Kozar <kozar@kth.se>
2026-09-11 11:47:00 +0200

    Added a CD pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "org.kth.countryguesser"
         minSdk = 33
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 1
+        versionName = System.getenv("VERSION_NAME") ?: "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
